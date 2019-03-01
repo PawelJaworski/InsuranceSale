ec38c91303003c3f77b41b4fffb63350cfb7da9d
Pawel <pawel.javorski@gmail.com>
2019-03-01 11:57:41 +0100

    frontend - bootstrap; error messages

--- frontend/src/main/ts/view/InsuranceForm.tsx
+++ frontend/src/main/ts/view/InsuranceForm.tsx
@@ -2,24 +2,42 @@ import * as React from "react";
 
 export default class InsuranceForm extends React.Component <{}> {
     state = {
-        message: '',
+        error: '',
     }
 
     componentDidMount(): void {
         const eventSource = new EventSource("/insurance/created/1");
-        eventSource.onopen = (event: MessageEvent) => console.log('open', event);
+        eventSource.onopen = (event: MessageEvent) => {
+            this.state.error = ""
+            this.setState({error: this.state.error})
+        }
+
         eventSource.onmessage = (event: MessageEvent) => {
-            this.state.message = event.data;
-            this.setState({message: this.state.message});
+            this.state.error = event.data;
+            this.setState({error: this.state.error});
         };
-        eventSource.onerror = (event: any) => console.log('error', event);
+        eventSource.onerror = (error: any) => {
+            this.state.error = "Policy service error"
+            this.setState({error: this.state.error})
+        }
     }
 
+    private tryInit() {
+
+    }
     render() {
         return (
-            <div>
-                <div>{this.state.message}</div>
-                <h1>Insurance Criteria</h1>
+            <div className="container">
+                <div className="row centered">
+                    <div className="col-md-6 offset-md-3 text-center">
+                        {this.state.error
+                            ? <div className="alert alert-danger">{this.state.error}</div>
+                            : null
+                        }
+
+                        <h1>Insurance Criteria</h1>
+                    </div>
+                </div>
             </div>
         );
     }
